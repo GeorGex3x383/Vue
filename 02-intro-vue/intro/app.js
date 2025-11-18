9ed46afe99f9679c79fdd45b392cdc96d76852d6
GeorGex3x383 <gjorgeemilio@gmail.com>
2025-11-18 12:29:16 -0600

    Subiendo sección 3 introducción a Vue

---
+++ 02-intro-vue/intro/app.js
@@ -0,0 +1,31 @@
+const {createApp, ref} = Vue;
+
+const app = createApp({
+    // template:`
+    // <h1>{{message}}</h1>
+    // <p>{{author}}</p>
+    // `,
+
+    setup(){
+        const message = ref("I'm Batman");
+        const author = ref("Bruce Wayne");
+
+        const changeQuote = () => {
+            message.value = 'Hola, soy Goku';
+            author.value = 'Goku';
+        }
+
+        // setTimeout(() => {
+        //     message.value = 'Soy Goku';
+        //     author.value = 'author Goku';
+        // }, 1000);
+
+        return {
+            message, 
+            author,
+            changeQuote,
+        }
+    }
+});
+
+app.mount('#myApp');
