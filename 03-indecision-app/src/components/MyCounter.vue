05ac32efc1d6bdaee3cc8586f16e01966ae1ff84
GeorGex3x383 <gjorgeemilio@gmail.com>
2025-11-19 13:33:21 -0600

    Subiendo sección 4 contador en Vue

---
+++ 03-indecision-app/src/components/MyCounter.vue
@@ -0,0 +1,42 @@
+<template>
+    <section>
+        <h3>Counter: {{ counter }}</h3>
+        <h3>Square: {{ squareCounter}}</h3>
+
+        <div>
+            <button class="btn" @click="counter++">+1</button>
+            <button class="btn" @click="counter--">-1</button>
+        </div>
+    </section>
+</template>
+
+<script lang="ts" setup>
+import { useCounter } from '@/composables/useCounter';
+
+
+const {counter, squareCounter} = useCounter(10);
+
+//import { computed, ref } from 'vue';
+
+interface Props{
+    value: number;
+}
+
+const props = defineProps<Props>();
+
+// //Mi solución
+// const buttonPlus = () => {
+//     return counter.value = counter.value + 1;
+// }
+
+// const buttonMinus = () => {
+//     return counter.value = counter.value - 1;
+// }
+</script>
+
+<style scoped>
+@reference "@/styles.css"; 
+.btn{
+    @apply p-5 bg-blue-500 rounded hover:bg-blue-700 mr-2;
+}
+</style>
---
+++ 03-indecision-app/src/composables/useCounter.ts
@@ -0,0 +1,13 @@
+import { computed, ref } from "vue";
+
+
+export const useCounter = (initialValue: number = 5) => {
+    const counter = ref(initialValue);
+
+    return{
+        counter,
+
+        //Read-only
+        squareCounter: computed(()=> counter.value*counter.value),
+    };
+};
